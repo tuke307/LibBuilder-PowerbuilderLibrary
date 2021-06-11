6aa369f7605979c08fae9e4213fb56c8d163889d
Tony <tonymeissner70@gmail.com>
2021-06-11 08:37:56 +0200

    configure log

--- src/LibBuilder.WPF.App/MvxWpfSetup.cs
+++ src/LibBuilder.WPF.App/MvxWpfSetup.cs
@@ -1,8 +1,11 @@
 ﻿// project=LibBuilder.WPF.Core, file=MvxWpfSetup.cs, creation=2020:8:24 Copyright (c) 2020
 // Timeline Financials GmbH & Co. KG. All rights reserved.
+using Data;
 using LibBuilder.WPF.Core.Region;
+using MvvmCross.Logging;
 using MvvmCross.Platforms.Wpf.Presenters;
 using MvvmCross.ViewModels;
+using Serilog;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Windows.Controls;
@@ -11,12 +14,24 @@ namespace LibBuilder.WPF.App
 {
     public class MvxWpfSetup<TApplication> : MvvmCross.Platforms.Wpf.Core.MvxWpfSetup<TApplication> where TApplication : class, IMvxApplication, new()
     {
+        public override MvxLogProviderType GetDefaultLogProviderType() => MvxLogProviderType.Serilog;
+
         public override IEnumerable<Assembly> GetViewAssemblies()
         {
             var list = new List<Assembly>();
             list.AddRange(base.GetViewAssemblies());
             list.Add(typeof(LibBuilder.WPF.Core.Views.MainWindow).Assembly);
             return list.ToArray();
+        }
+
+        protected override IMvxLogProvider CreateLogProvider()
+        {
+            Log.Logger = new LoggerConfiguration()
+              .MinimumLevel.Verbose()
+              .WriteTo.RollingFile(Constants.LogPath)
+              .CreateLogger();
+
+            return base.CreateLogProvider();
         }
 
         protected override IMvxWpfViewPresenter CreateViewPresenter(ContentControl root)
