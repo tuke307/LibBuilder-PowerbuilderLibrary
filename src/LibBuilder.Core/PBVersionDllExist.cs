435bc15e6f10dbf2137c18da84f345d17c21150d
Tony <tonymeissner70@gmail.com>
2021-06-11 09:26:44 +0200

    add class PBVersionDllExist

---
+++ src/LibBuilder.Core/PBVersionDllExist.cs
@@ -0,0 +1,22 @@
+﻿// project=LibBuilder.Core, file=PBVersionDllExist.cs, creation=2021:6:11 Copyright (c)
+// 2021 Timeline Financials GmbH & Co. KG. All rights reserved.
+using PBDotNetLib.orca;
+
+namespace LibBuilder.Core
+{
+    public class PBVersionDllExist
+    {
+        public bool DllExist { get; private set; }
+
+        public int PBVersion { get; private set; }
+
+        public PBVersionDllExist(PBDotNetLib.orca.Orca.Version? version)
+        {
+            if (version.HasValue)
+            {
+                this.PBVersion = (int)version.Value;
+                this.DllExist = Utils.CheckLibrary(version.GetAttributeOfType<DllNameAttribute>().DllName);
+            }
+        }
+    }
+}
