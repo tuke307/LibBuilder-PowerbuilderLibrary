e6b387adad6f549c0e526cad6be5771d2a1fddd9
Tony <tonymeissner70@gmail.com>
2021-06-11 09:26:31 +0200

    check dll library function, extension fo enums to get their attributes

--- src/LibBuilder.Core/Utils.cs
+++ src/LibBuilder.Core/Utils.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 
@@ -17,6 +18,29 @@ namespace LibBuilder.Core
         [DllImport("Kernel32.dll")]
         public static extern bool AttachConsole(int processId);
 
+        public static bool CheckLibrary(string fileName)
+        {
+            return LoadLibrary(fileName) == IntPtr.Zero;
+        }
+
+        /// <summary>
+        /// Gets an attribute on an enum field value
+        /// </summary>
+        /// <typeparam name="T">The type of the attribute you want to retrieve</typeparam>
+        /// <param name="enumVal">The enum value</param>
+        /// <returns>The attribute of type T that exists on the enum value</returns>
+        /// <example>
+        /// <![CDATA[string desc =
+        /// myEnumVariable.GetAttributeOfType<DescriptionAttribute>().Description;]]>
+        /// </example>
+        public static T GetAttributeOfType<T>(this Enum enumVal) where T : System.Attribute
+        {
+            var type = enumVal.GetType();
+            var memInfo = type.GetMember(enumVal.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+            return (attributes.Length > 0) ? (T)attributes[0] : null;
+        }
+
         /// <summary>
         /// Determines whether [is null or empty].
         /// </summary>
@@ -30,5 +54,8 @@ namespace LibBuilder.Core
         {
             return enumerable == null || !enumerable.Any();
         }
+
+        [DllImport("kernel32", SetLastError = true)]
+        private static extern IntPtr LoadLibrary(string lpFileName);
     }
 }
